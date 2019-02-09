caab6eb5754b1c534b41a121149e44afb0eebc57
Omri Gotlieb <omrigotlieb@gmail.com>
2019-02-09 12:39:37 +0200

    Minesweeper | Added babel, last tuning, use game and board modules

---
+++ CodeAcademy/minesweeper/lib/minesweeper.js
@@ -0,0 +1,9 @@
+//
+//
+// const g = new Game(3,3,4);
+// g.playMove(1,1);
+// //flipTile(playerBoard, bombBoard, 1,2)
+//
+// // console.log('Updated Player Board:');
+// // printBoard(playerBoard);
+"use strict";
